f477927ec07a37f4ed4c88c0c098a6e9c5232eff
KeyboardFire <andy@keyboardfire.com>
2015-02-16 11:17:53 -0600

    re-fxed bug (this is the last time, I promise)

--- client.user.js
+++ client.user.js
@@ -12,9 +12,14 @@ window.addEventListener('load', function() {
     if (typeof jQuery === 'undefined') {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.onload = function() { go(jQuery); };
+        script.addEventListener('load', function() {
+            var script2 = document.createElement('script');
+            script2.type = 'text/javascript';
+            script2.text = '(' + go + ')(jQuery);';
+            document.head.appendChild(script2);
+        });
         script.src = 'http://code.jquery.com/jquery-1.11.2.min.js';
-        document.body.appendChild(script);
+        document.head.appendChild(script);
     } else go(jQuery);
 });
 
@@ -28,36 +33,36 @@ function go($) { $(function() {
             });
         }
     });
-}); }
 
-function editInVim(val, callback) {
-    $.get('http://127.0.0.1:8000/server.php?str=' + encodeURIComponent(val));
-    makeOverlay(function(e) {
-        $.get('http://127.0.0.1:8000/server.php', function(data) {
-            callback(data);
+    function editInVim(val, callback) {
+        $.get('http://127.0.0.1:8000/server.php?str=' + encodeURIComponent(val));
+        makeOverlay(function(e) {
+            $.get('http://127.0.0.1:8000/server.php', function(data) {
+                callback(data);
+            });
         });
-    });
-}
+    }
 
-function makeOverlay(callback) {
-    var overlay = $('<div>')
-        .css({
-            position: 'fixed',
-            top: '0px',
-            left: '0px',
-            width: window.innerWidth,
-            height: window.innerHeight,
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            textAlign: 'center',
-            lineHeight: Math.floor(window.innerHeight / 2) + 'px'
-        })
-        .append($('<button>')
-            .text('Done')
-            .click(function(e) {
-                overlay.fadeOut(300, overlay.remove);
-                callback(e);
+    function makeOverlay(callback) {
+        var overlay = $('<div>')
+            .css({
+                position: 'fixed',
+                top: '0px',
+                left: '0px',
+                width: window.innerWidth,
+                height: window.innerHeight,
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                textAlign: 'center',
+                lineHeight: Math.floor(window.innerHeight / 2) + 'px'
             })
-        )
-        .hide().fadeIn(300)
-        .appendTo(document.body);
-}
+            .append($('<button>')
+                .text('Done')
+                .click(function(e) {
+                    overlay.fadeOut(300, overlay.remove);
+                    callback(e);
+                })
+            )
+            .hide().fadeIn(300)
+            .appendTo(document.body);
+    }
+}); }
